9bbb428ab3ea292df24a58ca2c28a1a61e75774e
Drew Hund <languageinvestigator@gmail.com>
2026-05-25 16:21:00 -0400

    set up sqlite db and set up CRUD actions

--- main.py
+++ main.py
@@ -1,26 +1,71 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
 
-all_books = []
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URL")
 
+db.init_app(app)
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
+    all_books = db.session.execute(db.select(Book).order_by(Book.title)).scalars()
     return render_template("index.html", books=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": f"{request.form['rating']}/10"
-        }
-        all_books.append(new_book)
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template("add.html")
+
+
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit_book(id):
+    book = db.get_or_404(Book, id)
+    if request.method == "GET":
+        return render_template("show.html", book=book)
+    else:
+        book.rating = float(request.form["rating"])
+        db.session.commit()
+        return redirect(url_for('home'))
+
+
+@app.route("/delete_book/<int:id>")
+def delete_book(id):
+    book = db.get_or_404(Book, id)
+
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for('home'))
+
 
 
 if __name__ == "__main__":
